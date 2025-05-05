ff397bbe18410332e08e3164363ece3a58ed8312
Franziska <Franziska.Roder@gmx.de>
2025-05-05 14:53:33 +0200

    checkForWinner

--- index.js
+++ index.js
@@ -9,44 +9,30 @@ Players = {
 }
 Logic = {
     winCondition: [
-        [
-            'name','name','name',
-            '','','',
-            '','',''
-        ], [
-            '','','',
-            'name','name','name',
-            '','',''
-        ], [
-            '','','',
-            '','','',
-            'name','name','name',
-        ],[
-            'name','','',
-            'name','','',
-            'name','','',
-        ], [
-            '','name','',
-            '','name','',
-            '','name','',
-        ], [
-            '','','name',
-            '','','name',
-            '','','name',
-        ], [
-            'name','','',
-            '','name','',
-            '','','name',
-        ], [
-            '','','name',
-            '','name','',
-            'name','','',
-        ]
-
-        
-     ]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
 }
 
+
+
+
+function checkForWinner() {
+    for (let condition of Logic.winCondition) {
+        const [a, b, c] = condition;
+        if (Game.board[a] && Game.board[a] === Game.board[b] && Game.board[a] === Game.board[c]) {
+            console.log(`${Game.board[a]} wins!`);
+             ;
+        }
+    }
+    return Game.board.includes('') ? 'No winner yet.' : 'It\'s a draw!';
+}
 
 function createPlayer(name, symbol) {
     // const name = name
@@ -59,18 +45,14 @@ function createPlayer(name, symbol) {
     return { name, score, giveScore, placeStone }
 }
 
-function checkForWinner() {
-    
-}
-
-
 Players.Fred.placeStone(2)
 Players.Franzi.placeStone(5)
-Players.Fred.placeStone(8)
+Players.Fred.placeStone(7)
 Players.Franzi.placeStone(3)
 Players.Fred.placeStone(4)
 Players.Franzi.placeStone(6)
 Players.Fred.placeStone(1)
+checkForWinner()
 
 
 console.log(Game);
